bfa59a5576c4ae7b65221b05b8292f724a47c22f
Clement <clement.iliou2014@gmail.com>
2023-06-10 15:11:19 +0200

    Initial commit

---
+++ src/components/Flow.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import Controls from './Controls.vue'
+import { VueFlow, Elements } from '@braks/vue-flow'
+
+const initialElements: Elements = [
+  { id: '1', label: 'Node 1', position: { x: 100, y: 100 } },
+  { id: '2', label: 'Node 2', position: { x: 100, y: 200 } },
+  { id: 'e1-2', source: '1', target: '2' },
+]
+
+const elements = ref(initialElements)
+</script>
+<template>
+  <VueFlow v-model="elements">
+    <Controls />
+  </VueFlow>
+</template>
